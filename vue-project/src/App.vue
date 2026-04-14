43cfd1268482842c3a10618c3e31274732382123
Tugche Sezer <302237167@student.rocmondriaan.nl>
2026-04-14 19:50:28 +0200

    Calculator layout created with display and buttons
    I made the first layout of a calculator.
    - There is a display on the top. It shows the number.
    - I added number buttons from 0 to 9.
    - I also added operation buttons like +, -, x, and ÷.
    - There are also some extra buttons like C, %, +/-, and =.
    
    Right now the calculator only looks ready. It does not work yet.
    - The buttons do not have any function.
    This is just the first step of the project.

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,76 @@
-<script setup></script>
-
+//Wat we gaan op de website zien
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
+<div class="calculator">
+  <div class="display"> {{current || 0}} </div>
+  <div class="btn"> C </div>
+  <div class="btn"> +/- </div>
+  <div class="btn"> % </div>
+
+  <div class="btn operator"> ÷ </div>
+
+  <div class="btn"> 7 </div>
+  <div class="btn"> 8 </div>
+  <div class="btn"> 9 </div>
+  <div class="btn operator"> x </div>
+
+  <div class="btn"> 4 </div>
+  <div class="btn"> 5 </div>
+  <div class="btn"> 6 </div>
+  <div class="btn operator"> - </div>
+
+  <div class="btn"> 1 </div>
+  <div class="btn"> 2 </div>
+  <div class="btn"> 3 </div>
+  <div class="btn operator"> + </div>
+  
+  <div class="btn zero"> 0 </div>
+  <div class="btn"> . </div>
+  <div class="btn operator"> = </div>
+
+</div>
 </template>
 
-<style scoped></style>
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.calculator {
+  margin: 0 auto;
+  margin-top: 15vh;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 100px);
+  grid-auto-rows: minmax(80px, auto);
+  justify-content: center;
+}
+
+.display {
+  grid-column: 1 / 5;
+  display: center;
+  background: #ffffff;
+  color: #000000;
+  border: 1px solid black;
+}
+.zero {
+  grid-column: 1 / 3;
+}
+
+.btn {
+  background-color: #ffe4ec;
+  border: 1px solid black;
+}
+
+.btn:hover {
+  background-color: #f497b5;
+}
+
+.operator{
+  background-color: #cb2e62;
+  color: white;
+  justify-content: center;
+}
+
+</style>
